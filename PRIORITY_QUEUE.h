3eedd26dab06aee0529497c0be8024dfa6bdadc4
panagiotis dimitropoulos <162046043+Paltoudara@users.noreply.github.com>
2025-10-15 00:08:49 +0300

    Update PRIORITY_QUEUE.h

--- PRIORITY_QUEUE.h
+++ PRIORITY_QUEUE.h
@@ -6,78 +6,56 @@
 #include<cstdlib>
 #include<functional>
 #include<utility>
-#include<cassert>
-#include"Header.h"
+#include"Macros.h"
 _PANAGIOTIS_BEGIN
-template<class _Ty,class Compare=std::less<_Ty>>
-class priority_queue final{
+template<class _Ty, class Compare = std::less<_Ty>>
+class priority_queue final {
 private:
 	//
-	std::vector<_Ty>_heap;
-	Compare _comp;
+	std::vector<_Ty>_heap{};
+	Compare _comp{};
 	//
+	//sink function done
 	void sink(std::size_t k)
 	{
-		static_assert(std::is_swappable_v<_Ty>
-			,"the type must be swappable");
-		//note that the compare func should be a func that takes two 
+		//note that the compare function should be a function that takes two 
 		//const _Ty& args and returns a bool,or else the behavior is 
 		//undefined
 		while (2 * k + 1 < _heap.size()) {
 			std::size_t j{ 2 * k + 1 };
 			if (j + 1 < _heap.size() && _comp(
-				std::as_const(_heap[j]) ,std::as_const(_heap[j + 1])))j++;
-			if (!_comp(std::as_const(_heap[k]) ,std::as_const( _heap[j])))break;
+				std::as_const(_heap[j]), std::as_const(_heap[j + 1])))j++;
+			if (!_comp(std::as_const(_heap[k]), std::as_const(_heap[j])))break;
 			std::swap(_heap[k], _heap[j]);
 			k = j;
 		}
 	}
+	//swim func done
 	void swim(std::size_t k)
 	{
-		static_assert(std::is_swappable_v<_Ty>, "the type must"
-			"be swappable");
-		//note that the compare func should be a func that takes two 
+		//note that the compare function should be a function that takes two 
 		//const _Ty& args and returns a bool,or else the behavior is 
 		//undefined
 		while (k > 0 && _comp(std::as_const(_heap[(k - 1) / 2]),
-			std::as_const( _heap[k]))) {
-			std::swap(_heap[k], _heap[(k - 1) / 2]);
+			std::as_const(_heap[k]))) {
+			std::swap(_heap[(k-1)/2], _heap[k]);
 			k = (k - 1) / 2;
 		}
 	}
 public:
 	//default constructor done
-	priority_queue()noexcept(
-		std::is_nothrow_default_constructible_v<std::vector<_Ty>>
-		&&std::is_nothrow_default_constructible_v<Compare>)
-		:_heap{}, _comp{}
-	{
-		static_assert(std::is_default_constructible_v<std::vector<_Ty>>
-			&& std::is_default_constructible_v<Compare>
-			,"the heap and comparator must be default constructible");
-	}
+	priority_queue() = default;
 	//comparator constructor done
-	explicit priority_queue(const Compare& comp)noexcept(
-		std::is_nothrow_default_constructible_v<std::vector<_Ty>>
-		&&std::is_nothrow_copy_constructible_v<Compare>)
+	explicit priority_queue(const Compare& comp)
+		noexcept(std::is_nothrow_copy_constructible_v<Compare>)
 		:_heap{},_comp{comp}
-	{
-		static_assert(std::is_default_constructible_v<std::vector<_Ty>>
-			&& std::is_copy_constructible_v<Compare>,
-			"the heap must be default constructible and the comp must"
-			"be copy constructible");
-	}
+	{}
 	//constructor with comp& and heap& done
 	priority_queue(const Compare& comp, const std::vector<_Ty>& heap)
-		:_comp{comp},_heap{heap}
+		:_comp{ comp }, _heap{ heap }
 	{
-		static_assert(std::is_copy_constructible_v<Compare>
-			&& std::is_copy_constructible_v<std::vector<_Ty>>,
-			"the heap must be copy constructible and the comp must"
-			"be copy constructible");
-		//if copy fails no object created
 		//if all goes good we make heap
-		
+
 		//if the func sink throws we are in partially modified or not heap
 		//or else the we have the heap order we want 
 		//call the func sink to create the heap order
@@ -85,14 +63,10 @@ public:
 			sink(k);
 		}
 	}
+	//constructor with &comp and &&heap done
 	priority_queue(const Compare& comp, std::vector<_Ty>&& heap)
-		:_comp{comp},_heap{std::move(heap)}
+		:_comp{ comp }, _heap{ std::move(heap) }
 	{
-		static_assert(std::is_copy_constructible_v<Compare>
-			&& std::is_move_constructible_v<std::vector<_Ty>>,"the comp"
-			"should be copy constructible and the vector should be"
-			"move constructible");
-		//if move fails no object created
 		//if all goes good we make heap
 
 		//if the func sink throws we are in partially modified or not heap
@@ -102,74 +76,78 @@ public:
 			sink(k);
 		}
 	}
-	priority_queue(const priority_queue<_Ty, Compare>& other) 
-		:_comp{}, _heap{}
-	{
-		static_assert(std::is_default_constructible_v<Compare>&&
-			std::is_default_constructible_v<std::vector<_Ty>>,"the"
-			"comp must be default constructible and the heap must be"
-			"default constructible");
-		static_assert(std::is_copy_assignable_v<Compare> &&
-			std::is_copy_assignable_v<std::vector<_Ty>>, "the"
-			"comp must be copy assignable and the heap must be"
-			"copy assignable");
-		if (this != &other) {
-			_comp = other._comp;
-			_heap = other._heap;
-		}
-	}
-	priority_queue(priority_queue<_Ty, Compare>&& other) 
-		:_comp{}, _heap{}
-	{
-		static_assert(std::is_default_constructible_v<Compare> &&
-			std::is_default_constructible_v<std::vector<_Ty>>, "the"
-			"comp must be default constructible and the heap must be"
-			"default constructible");
-		static_assert(std::is_move_assignable_v<Compare> &&
-			std::is_move_assignable_v<std::vector<_Ty>>, "the"
-			"comp must be move assignable and the heap must be"
-			"move assignable");
-		if (this != &other) {
-			_comp =std::move( other._comp);
-			_heap = std::move(other._heap);
-		}
-	}
+	//copy constructor done
+	priority_queue(const priority_queue<_Ty, Compare>& other)
+		:_comp{other._comp}, _heap{other._heap} {}
+	//move constructor done
+	priority_queue(priority_queue<_Ty, Compare>&& other)
+		noexcept(std::is_nothrow_move_constructible_v<Compare>
+			&&std::is_nothrow_move_constructible_v<std::vector<_Ty>>)
+		:_comp{std::move(other._comp)}, _heap{std::move(other._heap)}{}
 	bool empty()const noexcept {
 		return _heap.empty();
 	}
 	std::size_t size()const noexcept {
 		return _heap.size();
 	}
-	const _Ty& top()const noexcept {
+	const _Ty& top()const noexcept(noexcept(_heap.front())) {
 		return _heap.front();
 	}
 	void push(const _Ty& val) {
 		_heap.push_back(val);
-		swim(_heap.size() - 1);
+		swim(_heap.size() - 1);//safe because _heap.size()>=1
 	}
 	void push(_Ty&& val) {
 		_heap.push_back(std::move(val));
-		swim(_heap.size()-1);//safe because _heap.size()>=1
+		swim(_heap.size() - 1);//safe because _heap.size()>=1
 	}
 	template<class ..._Valty>
 	void emplace(_Valty&&..._Val) {
 		_heap.emplace_back(std::forward<_Valty>(_Val)...);
-		swim(_heap.size() - 1);
+		swim(_heap.size() - 1);//safe because _heap.size()>=1
 	}
 	void pop() {
 		//throw exception if the heap has no elements
-		static_assert(std::is_swappable_v<_Ty>,"the type _Ty must be"
-			"swappable");
-		std::swap(_heap[0],_heap [_heap.size()-1]);
-		_heap.pop_back();
-		sink(0);
+		if (_heap.size() != 0) {
+			std::swap(_heap[0], _heap[_heap.size() - 1]);
+			_heap.pop_back();
+			sink(0);
+		}
 	}
-	void swap(priority_queue<_Ty,Compare>&other)
+	void swap(priority_queue<_Ty, Compare>& other)
 		noexcept(std::is_nothrow_swappable_v<Compare>&&
 			std::is_nothrow_swappable_v<std::vector<_Ty>>)
 	{
-		std::swap(_comp, other._comp);
-		std::swap(_heap, other._heap);
+			std::swap(_comp, other._comp);
+			std::swap(_heap, other._heap);
+	}
+	priority_queue<_Ty, Compare>& operator=(const priority_queue<_Ty, Compare>& other)& {
+		if (this != &other) {
+			_comp = other._comp;
+			_heap = other._heap;
+		}
+		return *this;
+	}
+	priority_queue<_Ty, Compare>& operator=(priority_queue<_Ty, Compare>&& other)
+		&noexcept(std::is_nothrow_move_assignable_v<Compare>
+			&&std::is_nothrow_move_assignable_v<std::vector<_Ty>>)
+	{
+		if (this != &other) {
+			_comp = std::move(other._comp);
+			_heap = std::move(other._heap);
+		}
+		return *this;
+	}
+	~priority_queue() {
+		if (!_heap.empty()) {
+			_heap.~vector();
+		}
+		_comp.~Compare();
+	}
+	void show()const {
+		for (std::size_t i = 0; i < _heap.size(); i++) {
+			std::cout << _heap[i] << '\n';
+		}
 	}
 };
 
